d41aff45c5ce1e99a530a8fc42eeace63bdf1ce6
hosseindq <118542161+hosseindq@users.noreply.github.com>
2022-11-18 18:38:07 +0330

    Add files via upload

---
+++ project22-.php
@@ -0,0 +1,11 @@
+<?php
+if($_POST['hossein'] == -1){
+   echo $_POST['hossein'] . "<br>";
+   echo "nmi toni add mard koni!";
+}
+else{
+    echo $_POST['hossein'] . "<br>";
+ echo "add jajdid ravad konid:";
+}
+
+?>
